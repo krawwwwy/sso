758be9cbf41d5ca68ef14a41fcfff34946bde0f2
krawwwwy <krakenlex@mail.ru>
2025-07-03 18:12:03 +0300

    add postgresql implemention data layer

---
+++ internal/storage/postgresql/postgresql.go
@@ -0,0 +1,113 @@
+package postgresql
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"sso/internal/domain/models"
+	"sso/internal/storage"
+
+	"github.com/lib/pq"
+	_ "github.com/lib/pq" // драйвер для PostgreSQL
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+const emptyID = 0
+
+// New creates a new Storage instance
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.postgresql.New"
+
+	db, err := sql.Open("postgres", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	// Проверяем соединение
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+// SaveUser saves user by email and password and returns his id
+func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (int64, error) {
+	const op = "storage.postgresql.SaveUser"
+
+	stmt, err := s.db.Prepare("INSERT INTO users(email, pass_hash) VALUES($1, $2) RETURNING id")
+	if err != nil {
+		return emptyID, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, email, passHash)
+
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			return emptyID, storage.ErrUserExists
+		}
+		return emptyID, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return emptyID, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+// User returns user by email
+func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
+	const op = "storage.postgresql.User"
+
+	var user models.User
+
+	row := s.db.QueryRowContext(ctx, "SELECT id, email, pass_hash FROM users WHERE email = $1", email)
+
+	err := row.Scan(&user.ID, &user.Email, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
+}
+
+func (s *Storage) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "storage.postgresql.IsAdmin"
+
+	var isAdmin bool
+
+	err := s.db.QueryRowContext(ctx, "SELECT 1 FROM admins WHERE user_id = $1", userID).Scan(&isAdmin)
+
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
+}
+
+func (s *Storage) App(ctx context.Context, appID int) (models.App, error) {
+	const op = "storage.postgresql.App"
+
+	var app models.App
+
+	row := s.db.QueryRowContext(ctx, "SELECT id, name, secret FROM apps WHERE id = $1", appID)
+
+	err := row.Scan(&app.ID, &app.Name, &app.Secret)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.App{}, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
+		}
+		return models.App{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return app, nil
+}
